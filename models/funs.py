4c9417f82260d56701b540d27ab4d6bf6b4e9392
EryiXie <yauxu0325@gmail.com>
2021-12-09 12:04:46 +0100

    who knows

--- models/funs.py
+++ models/funs.py
@@ -10,15 +10,17 @@ def Euclidean2Sphere(normal_euclidean):
 
     theta = torch.atan2(y, x) / (2 * math.pi) + 0.5 # [-pi, pi] -> [0,1]
     phi = torch.atan2(torch.sqrt(x*x + y*y), z) / math.pi # [0, pi] -> [0, 1]
+    valid_mask = (x*x + y*y + z*z)>0
 
-    normal_2dsphere = torch.stack([theta, phi], dim=1)
+    normal_2dsphere = torch.stack([theta, phi, valid_mask], dim=1)
     return normal_2dsphere
 
 def Sphere2Euclidean(normal_2dsphere):
     theta = (normal_2dsphere[:, 0, :, :] - 0.5)*2*math.pi
     phi = normal_2dsphere[:, 1, :, :]*math.pi
+    valid_mask = (normal_2dsphere[:, 2, :, :] > 0.3).unsqueeze(dim=1).repeat(1,3,1,1)
     
-    valid_mask = torch.logical_or((theta > 1e-2), (phi > 1e-2)).unsqueeze(dim=1).repeat(1,3,1,1)
+    #valid_mask = torch.logical_or((theta > 1e-2), (phi > 1e-2)).unsqueeze(dim=1).repeat(1,3,1,1)
     x = torch.sin(phi)*torch.cos(theta)
     y = torch.sin(phi)*torch.sin(theta)
     z = torch.cos(phi)
--- models/losses.py
+++ models/losses.py
@@ -21,18 +21,20 @@ class Loss(nn.Module):
     def forward(self, net, normal_preds, gt_normals):
         # normal loss
         gt_normals = Variable(gt_normals, requires_grad=False)
+        valid_mask_gt = torch.norm(gt_normals, p=2, dim=1)
         gt_normals = Euclidean2Sphere(gt_normals)
-        point_wise_loss = self.pts_loss(normal_preds, gt_normals)
+        point_wise_loss = self.pts_loss(normal_preds, gt_normals, valid_mask_gt)
         return {'point': point_wise_loss}
 
 class circle_loss(nn.Module):
     def __init__(self):
         super(circle_loss, self).__init__()
     
-    def forward(self, normal_preds, gt_normals):
+    def forward(self, normal_preds, gt_normals, valid_mask_gt):
         B, C, H, W = normal_preds.shape
         term_phi = torch.abs(normal_preds[:,1,:,:] - gt_normals[:,1,:,:])
         term_theta = torch.abs(normal_preds[:,0,:,:] - gt_normals[:,0,:,:])
         term_theta = 2 * torch.minimum(term_theta, 1 - term_theta)
-        loss = (term_phi + term_theta)
+        term_valid = torch.abs(normal_preds[:,2,:,:] - valid_mask_gt)
+        loss = term_phi + term_theta + term_valid
         return loss.mean()
